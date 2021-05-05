1b372f2eede5583dbd3df818654ac88f52822d41
Mitrajit Chandra Chandra <chandra.rupam@gmail.com>
2021-05-05 13:25:12 +0530

    Code cleaned up and status format changed

--- tweet.py
+++ tweet.py
@@ -1,66 +1,18 @@
-import sys, os
-# import encryptdecrypt38
+import os
 import covid19api
 import tweepy
-# def delete_tweet() :
-#     tweet = api.user_timeline(count = 1)[0]
-#     print("Are you sure you want to delete this tweet(Y/N):\n",tweet.text.lstrip())
-#     confirm = input('')
-#     if confirm =='Y' or confirm == 'y':
-#         api.destroy_status(tweet.id)
-#         playsound('delete-tweet.mp3')
-#         print('Tweet is deleted')
-#     else:
-#         print('Tweet is not deleted')
-# my_parser = argparse.ArgumentParser(description='List the content of a folder')
-# my_parser.add_argument('tweet',
-#                        metavar='tweet',
-#                        type=str,
-#                        action="append",
-#                        nargs="*",
-#                        help='tweet <The text you want to tweet>')
-# my_parser.add_argument('-api',
-#                        type=str,
-#                        metavar=("<api key>", "<api secret>" , "<token key>","<token secret>"),
-#                        action="store",
-#                        help='gets the api keys and secrets',
-#                        nargs=4)
-# my_parser.add_argument('-tr',
-#                        type=str,
-#                        metavar=("place name or global"),
-#                        action="store",
-#                        help='gets the trending in the given place',
-#                        nargs="*")
-# my_parser.add_argument('-d',
-#                        action="store_true",
-#                        help='deletes the last tweet')
-# args = my_parser.parse_args()
-# if len(args.tweet[0])==0 and args.api is None and not args.d and args.tr is None:
-#     my_parser.print_help()
-#     sys.exit(0)
-# if args.api is not None:
-#     encryptdecrypt38.encrypt([args.api[0],args.api[1],args.api[2],args.api[3]])
-#     print('API, tokens keys and secrets are saved')
-# cred=encryptdecrypt38.decrypt()
 auth = tweepy.OAuthHandler(os.getenv("API_KEY"), os.getenv("SECRET_KEY"))
 auth.set_access_token(os.getenv("ACCESS_TOKEN_KEY"), os.getenv("ACCESS_TOKEN_SECRET"))
 print("OAuthHandler set")
 api = tweepy.API(auth)
 print("logged in as @"+api.me().screen_name)
-# except IndexError:
-#     my_parser.print_help()
-#     sys.exit(0)
-# elif len(args.tweet[0])!=0 :
-#     api.update_status(status = " ".join(args.tweet[0]))
-#     playsound('tweet-sfx.mp3')
-#     print("Tweeted")
 
 for tweet in api.user_timeline(count=2, include_rts = False):
     if "COVID tracker bot" in tweet.text:
         api.destroy_status(tweet.id)
 
-api.update_status("".join(["🦠 COVID tracker bot 🤖\n","😷 Active cases: ",covid19api.active,"\n😄 Recovery rate: ",
+api.update_status("".join(["🦠 COVID tracker bot • India🤖\n","😷 Active cases: ",covid19api.active,"\n😄 Recovery rate: ",
 covid19api.recoverypercent,"\n😖 Fatality rate: ",covid19api.fatalitypercent,"\n💉 Vaccination rate: ",
 covid19api.vaccinatedpercent,"\n📈 Today ",covid19api.today_inc if int(covid19api.today_inc) < 0 else "+"+
 covid19api.today_inc,"\n📈 Yesterday ",covid19api.yesterday_inc if int(covid19api.yesterday_inc)<0 else "+"
-+covid19api.yesterday_inc,"\nLast update ",covid19api.last_update]))
++covid19api.yesterday_inc,"\nLast update ",covid19api.last_update,"#IndiaFightsCOVID19 #COVIDEmergency #CovidTracker"]))
